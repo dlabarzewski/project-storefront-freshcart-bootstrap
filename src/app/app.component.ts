2a99ecceb2bd9821597d1aef79e97422f6fe19b3
Dawid Labarzewski <dawid.labarzewski@gmail.com>
2023-01-17 20:37:54 +0100

    feat: create layout header with categories

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CategoryModel } from './models/category.model';
+import { CategoryService } from './services/category.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-freshcard-bootstrap-theme';
+  readonly categories$: Observable<CategoryModel[]> = this._categoryService.getAll();
+
+  constructor(private _categoryService: CategoryService) {
+  }
 }
