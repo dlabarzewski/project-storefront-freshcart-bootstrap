7e837d2cccd53f9703ea94114b70d5de251eb466
Dawid Labarzewski <dawid.labarzewski@gmail.com>
2023-01-21 21:00:28 +0100

    feat: category products pagination

--- src/app/components/category-products/category-products.component.ts
+++ src/app/components/category-products/category-products.component.ts
@@ -1,16 +1,17 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
-import { Observable, combineLatest, of, map } from 'rxjs';
+import { BehaviorSubject, Observable, combineLatest, of } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+import { CategoryProductsSortQueryModel } from '../../query-models/category-products-sort.query-model';
+import { CategoryProductsFiltersQueryModel } from '../../query-models/category-products-filters.query-model';
 import { CategoryProductsQueryModel } from '../../query-models/category-products.query-model';
 import { CategoryModel } from '../../models/category.model';
 import { ProductModel } from '../../models/product.model';
-import { CategoryProductsSortQueryModel } from '../../query-models/category-products-sort.query-model';
 import { CategoryService } from '../../services/category.service';
 import { ProductService } from '../../services/product.service';
 import { CategoryProductsCategoryQueryModel } from '../../query-models/category-products-category.query-model';
 import { CategoryProductsProductQueryModel } from '../../query-models/category-products-product.query-model';
 import { ProductSorting } from 'src/app/statics/product-sorting.static';
-import { CategoryProductsFiltersQueryModel } from 'src/app/query-models/category-products-filters.query-model';
 
 @Component({
   selector: 'app-category-products',
@@ -21,6 +22,8 @@ import { CategoryProductsFiltersQueryModel } from 'src/app/query-models/category
 })
 export class CategoryProductsComponent {
 
+  private _defaultLimit: number = 5;
+
   private _sortings$: Observable<CategoryProductsSortQueryModel[]> = of([
     { id: ProductSorting.featureValueDesc, name: 'Featured' },
     { id: ProductSorting.priceAsc, name: 'Price: Low to High' },
@@ -28,17 +31,26 @@ export class CategoryProductsComponent {
     { id: ProductSorting.ratingValueDesc, name: 'Avg. Rating' }
   ]);
 
+  private _pageSizeOptions$: Observable<number[]> = of([5, 10, 15]);
+
+  private _pagesSubject: BehaviorSubject<number[]> = new BehaviorSubject<number[]>([]);
+  public pages$: Observable<number[]> = this._pagesSubject.asObservable();
+
   private _filters$: Observable<CategoryProductsFiltersQueryModel> = combineLatest([
     this._activatedRoute.params,
     this._activatedRoute.queryParams,
-    this._sortings$
+    this._sortings$,
+    this._pageSizeOptions$
   ]).pipe(
     map(
-      ([params, queryParams, sortings]: [Params, Params, CategoryProductsSortQueryModel[]]) => ({
+      ([params, queryParams, sortings, pageSizeOptions]: [Params, Params, CategoryProductsSortQueryModel[], number[]]) => ({
         categoryId: params['categoryId'],
         sort: queryParams['sort'] !== undefined ? +queryParams['sort'] : ProductSorting.featureValueDesc,
-        sortings
-    }))
+        limit: queryParams['limit'] !== undefined ? +queryParams['limit'] : this._defaultLimit,
+        page: queryParams['page'] !== undefined ? +queryParams['page'] : 1,
+        sortings,
+        pageSizeOptions
+      }))
   );
 
   readonly model$: Observable<CategoryProductsQueryModel> = combineLatest([
@@ -61,33 +73,64 @@ export class CategoryProductsComponent {
 
     const value = (target as HTMLSelectElement).value;
 
+    this._navigate({ sort: value });
+  }
+
+  public onLimitChange(limit: number): void {
+    this._navigate({ limit: limit });
+  }
+
+  public onPageChange(page: number): void {
+    this._navigate({ page: page });
+  }
+
+  private _setPages(filters: CategoryProductsFiltersQueryModel, productsCount: number): void {
+    const maxPage = Math.ceil(productsCount / filters.limit);
+
+    const pages = Array.from(Array(maxPage).keys()).map(x => ++x);
+
+    this._pagesSubject.next(pages);
+
+    if (filters.page > maxPage) {
+      this._navigate({ page: maxPage })
+    }
+  }
+
+  private _navigate(queryParams: Object) {
     this._router.navigate(
       [],
       {
         relativeTo: this._activatedRoute,
-        queryParams: {
-          sort: value
-        },
+        queryParams: queryParams,
         queryParamsHandling: 'merge'
       }
     )
   }
 
   private _filterProducts(filters: CategoryProductsFiltersQueryModel, products: ProductModel[]): ProductModel[] {
+    return products.filter(product => product.categoryId === filters.categoryId);
+  }
+
+  private _sortProducts(filters: CategoryProductsFiltersQueryModel, products: ProductModel[]): ProductModel[] {
     const sortFunctionsMap = new Map([
-      [ProductSorting.featureValueDesc, (a: ProductModel, b: ProductModel) => { return b.featureValue - a.featureValue }],
-      [ProductSorting.priceAsc, (a: ProductModel, b: ProductModel) => { return a.price - b.price }],
-      [ProductSorting.priceDesc, (a: ProductModel, b: ProductModel) => { return b.price - a.price }],
-      [ProductSorting.ratingValueDesc, (a: ProductModel, b: ProductModel) => { return b.ratingValue - a.ratingValue }],
-    ])
+      [ProductSorting.featureValueDesc, (a: ProductModel, b: ProductModel) => b.featureValue - a.featureValue],
+      [ProductSorting.priceAsc, (a: ProductModel, b: ProductModel) => a.price - b.price],
+      [ProductSorting.priceDesc, (a: ProductModel, b: ProductModel) => b.price - a.price],
+      [ProductSorting.ratingValueDesc, (a: ProductModel, b: ProductModel) => b.ratingValue - a.ratingValue],
+    ]);
 
     const sortFunction = sortFunctionsMap.get(filters.sort) ?? sortFunctionsMap.get(ProductSorting.featureValueDesc);
-    
-    const categoryProducts = products.filter(product => product.categoryId === filters.categoryId);
 
-    const sortedProducts = categoryProducts.sort(sortFunction);
+    const sortedProducts = products.sort(sortFunction);
 
     return sortedProducts;
+  }
+
+  private _paginateProducts(filters: CategoryProductsFiltersQueryModel, products: ProductModel[]): ProductModel[] {
+    return products.slice(
+      (filters.page - 1) * filters.limit,
+      filters.page * filters.limit
+    );
   }
 
   private _mapQueryModel(
@@ -95,15 +138,23 @@ export class CategoryProductsComponent {
     categories: CategoryModel[],
     products: ProductModel[]
   ): CategoryProductsQueryModel {
-    const filteredProducts = this._filterProducts(filters, products);
+    products = this._filterProducts(filters, products);
+
+    products = this._sortProducts(filters, products);
+
+    const productsCount = products.length;
+
+    products = this._paginateProducts(filters, products);
+
+    this._setPages(filters, productsCount);
 
     const categoriesMap = categories.reduce((a, c) => ({ ...a, [c.id]: c }), {} as Record<string, CategoryModel>);
 
     return {
       category: categoriesMap[filters.categoryId] ? this._mapCategoryToQueryModel(categoriesMap[filters.categoryId]) : undefined,
       categories: categories.map(category => this._mapCategoryToQueryModel(category)),
-      products: filteredProducts.map(product => this._mapProductToQueryModel(product, categoriesMap[product.categoryId])),
-      productsCount: filteredProducts.length,
+      products: products.map(product => this._mapProductToQueryModel(product, categoriesMap[product.categoryId])),
+      productsCount: productsCount,
       filters: filters
     }
   }
