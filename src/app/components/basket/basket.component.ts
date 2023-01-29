14ee802b4d04511cf775a3feefb870245b1b5805
Dawid Labarzewski <dawid.labarzewski@gmail.com>
2023-01-29 19:00:09 +0100

    feat: basket products quantity change, store basket products in session storage

--- src/app/components/basket/basket.component.ts
+++ src/app/components/basket/basket.component.ts
@@ -5,6 +5,7 @@ import { BasketProductModel } from '../../models/basket-product.model';
 import { ProductService } from '../../services/product.service';
 import { BasketService } from '../../services/basket.service';
 import { BasketQueryModel } from 'src/app/query-models/basket.query-model';
+import { BasketProductQueryModel } from 'src/app/query-models/basket-product.query-model';
 
 @Component({
   selector: 'app-basket',
@@ -33,12 +34,52 @@ export class BasketComponent {
   constructor(private _productService: ProductService, private _basketService: BasketService) {
   }
 
+  public removeProduct(id: string): void {
+    this._basketService.removeProduct(id);
+  }
+
+  public onChangeProductQuantity(id: string, event: Event): void {
+    if (event.target === null) return;
+
+    const quantity = parseInt((event.target as HTMLInputElement).value);
+
+    if (isNaN(quantity)) return;
+
+    this._basketService.changeProductQuantity(id, quantity);
+  }
+
+  public decrementProductQuantity(product: BasketProductQueryModel): void {
+    if (product.quantity < 2) return;
+
+    this._basketService.changeProductQuantity(product.id, product.quantity - 1);
+  }
+
+  public incrementProductQuantity(product: BasketProductQueryModel): void {
+    this._basketService.changeProductQuantity(product.id, product.quantity + 1);
+  }
+
   private _mapQueryModel(allProducts: ProductModel[], basketProducts: BasketProductModel[]): BasketQueryModel {
+    const mappedProducts = this._mapProductsToQueryModel(allProducts, basketProducts);
+
+    const productsSubtotal = mappedProducts.reduce((a, c) => a += c.subtotal, 0);
+
+    const deliveryValue = productsSubtotal < this._freeDeliveryFrom ? this._defaultDeliveryValue : 0;
+
+    return {
+      products: mappedProducts,
+      subtotal: productsSubtotal + deliveryValue,
+      productsSubtotal,
+      deliveryValue,
+      leftToFreeDelivery: deliveryValue ? this._freeDeliveryFrom - productsSubtotal : 0
+    }
+  }
+
+  private _mapProductsToQueryModel(allProducts: ProductModel[], basketProducts: BasketProductModel[]): BasketProductQueryModel[] {
     const productsMap = allProducts.reduce((a, c) => ({ ...a, [c.id]: c }), {} as Record<string, ProductModel>);
 
     const availableProducts = basketProducts.filter(product => productsMap[product.id] !== undefined);
 
-    const mappedProducts = availableProducts.map(availableProduct => {
+    return availableProducts.map(availableProduct => {
       const product = productsMap[availableProduct.id];
 
       return {
@@ -50,17 +91,5 @@ export class BasketComponent {
         subtotal: product.price * availableProduct.quantity
       }
     });
-
-    const productsSubtotal = mappedProducts.reduce((a, c) => a += c.subtotal, 0);
-
-    const deliveryValue = productsSubtotal > this._freeDeliveryFrom ? 0 : this._defaultDeliveryValue;
-
-    return {
-      products: mappedProducts,
-      subtotal: productsSubtotal + deliveryValue,
-      productsSubtotal,
-      deliveryValue,
-      leftToFreeDelivery: deliveryValue ? this._freeDeliveryFrom - productsSubtotal : 0
-    }
   }
 }
--- src/app/services/basket.service.ts
+++ src/app/services/basket.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, map, Observable, shareReplay } from 'rxjs';
+import { BehaviorSubject, map, Observable, shareReplay, tap } from 'rxjs';
 import { BasketProductModel } from '../models/basket-product.model';
 
 @Injectable({ providedIn: 'root' })
 export class BasketService {
-  private _productsSubject: BehaviorSubject<BasketProductModel[]> = new BehaviorSubject<BasketProductModel[]>([]);
+  private _storageKey: string = 'basket';
+
+  private _productsSubject: BehaviorSubject<BasketProductModel[]> = new BehaviorSubject<BasketProductModel[]>(this._getProductsFromStorage());
   private _products$: Observable<BasketProductModel[]> = this._productsSubject.asObservable().pipe(
+    tap(
+      products => this._setProductsInStorage(products)
+    ),
     shareReplay(1)
   )
 
@@ -40,5 +45,43 @@ export class BasketService {
     });
 
     this._productsSubject.next(newProducts);
+  }
+
+  changeProductQuantity(id: string, quantity: number): void {
+    const newProducts = this._productsSubject.value.map(product => {
+      if (product.id !== id) return product;
+
+      return {
+        id,
+        quantity: quantity
+      }
+    });
+
+    this._productsSubject.next(newProducts);
+  }
+
+  removeProduct(id: string): void {
+    const newProducts = this._productsSubject.value.filter(product => product.id !== id);
+
+    this._productsSubject.next(newProducts);
+  }
+
+  private _getProductsFromStorage(): BasketProductModel[] {
+    const storageProducts = window.sessionStorage.getItem(this._storageKey);
+
+    if (storageProducts === null) return [];
+
+    try {
+      return JSON.parse(storageProducts);
+    }
+    catch(e) {
+      return [];
+    }
+  }
+
+  private _setProductsInStorage(products: BasketProductModel[]): void {
+    const storageProducts = JSON.stringify(products);
+
+    window.sessionStorage.setItem(this._storageKey, storageProducts);
   }
 }
